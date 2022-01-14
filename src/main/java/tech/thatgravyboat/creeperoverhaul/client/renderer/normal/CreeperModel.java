a7f65305ec990089bb74d6ac4f02e02f681da9b8
ThatGravyBoat <thatgravyboat@gmail.com>
2022-01-14 05:02:17 -0330

    Heads now rotate. Swamp new model. Fixed hill loot table.

--- src/main/java/tech/thatgravyboat/creeperoverhaul/client/renderer/normal/CreeperModel.java
+++ src/main/java/tech/thatgravyboat/creeperoverhaul/client/renderer/normal/CreeperModel.java
@@ -1,9 +1,14 @@
 package tech.thatgravyboat.creeperoverhaul.client.renderer.normal;
 
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 import tech.thatgravyboat.creeperoverhaul.common.entity.base.BaseCreeper;
 import tech.thatgravyboat.creeperoverhaul.common.entity.base.CreeperType;
+
+import java.util.List;
 
 public class CreeperModel<E extends BaseCreeper> extends AnimatedGeoModel<E> {
 
@@ -26,5 +31,18 @@ public class CreeperModel<E extends BaseCreeper> extends AnimatedGeoModel<E> {
     @Override
     public ResourceLocation getAnimationFileLocation(E entity) {
         return type.animation();
+    }
+
+
+    @Override
+    public void setLivingAnimations(E entity, Integer uniqueID, @Nullable AnimationEvent customPredicate) {
+        super.setLivingAnimations(entity, uniqueID, customPredicate);
+
+        if (customPredicate == null) return;
+        List<EntityModelData> extraDataOfType = customPredicate.getExtraDataOfType(EntityModelData.class);
+        var head = this.getAnimationProcessor().getBone("head");
+        if (head != null) {
+            head.setRotationY(extraDataOfType.get(0).netHeadYaw * ((float)Math.PI / 180F));
+        }
     }
 }
